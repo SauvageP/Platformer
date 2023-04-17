dd17375abad00f933a9f57395dc2bbf886a06e29
Perry G <SauvageP@users.noreply.github.com>
2023-04-16 23:45:07 -0500

    adds phyics to player

--- src/player.py
+++ src/player.py
@@ -1,50 +1,44 @@
 import pygame
-from pygame.sprite import Sprite
+
 from src.utils import load_image
 
 
-class Player(Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-
-        self.image = load_image("assets/images/player.png", colorkey=-1)
+        self.image = load_image("assets/images/player.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-
-        self.speed_x = 0
-        self.speed_y = 0
+        self.velocity = pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, 0.5)
+        self.jump_speed = -10
+        self.on_ground = False
 
     def update(self, platforms):
-        self.speed_y += 0.5
+        self.velocity += self.acceleration
+        self.rect.move_ip(self.velocity.x, self.velocity.y)
+        self.on_ground = False
 
-        self.rect.x += self.speed_x
-        self.collide(platforms, "x")
+        for platform in platforms:
+            if self.rect.colliderect(platform.rect):
+                if self.velocity.y > 0 and self.rect.bottom <= platform.rect.bottom + self.velocity.y:
+                    self.rect.bottom = platform.rect.top
+                    self.velocity.y = 0
+                    self.on_ground = True
+                elif self.velocity.y < 0 and self.rect.top >= platform.rect.top + self.velocity.y:
+                    self.rect.top = platform.rect.bottom
+                    self.velocity.y = 0
 
-        self.rect.y += self.speed_y
-        self.collide(platforms, "y")
+        if not self.on_ground:
+            self.velocity.y = min(self.velocity.y, 10)
 
     def move_left(self):
-        self.speed_x = -5
+        self.velocity.x = -5
 
     def move_right(self):
-        self.speed_x = 5
+        self.velocity.x = 5
 
     def jump(self):
-        self.speed_y = -12
-
-    def collide(self, platforms, axis):
-        for platform in pygame.sprite.spritecollide(self, platforms, False):
-            if axis == "x":
-                if self.speed_x > 0:
-                    self.rect.right = platform.rect.left
-                elif self.speed_x < 0:
-                    self.rect.left = platform.rect.right
-                self.speed_x = 0
-            elif axis == "y":
-                if self.speed_y > 0:
-                    self.rect.bottom = platform.rect.top
-                    self.speed_y = 0
-                elif self.speed_y < 0:
-                    self.rect.top = platform.rect.bottom
-                    self.speed_y = 0
+        if self.on_ground:
+            self.velocity.y = self.jump_speed
